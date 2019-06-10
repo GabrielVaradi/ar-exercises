788c51cb8a23a0467b4b0e697e199536582994a2
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-06-10 23:59:25 +0000

    Finish Exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,7 +8,7 @@ end
 
 
 store1 = Store.create(name: "Burnaby", mens_apparel: true, womens_apparel: true, annual_revenue: 300000)
-store2 = Store.create(name: "Richmond", mens_apparel: false, womens_apparel: true, annual_revenue: 190000)
-store3 = Store.create(name: "Gastown", mens_apparel: true, womens_apparel: false, annual_revenue: 126000)
+store2 = Store.create(name: "Richmond", mens_apparel: false, womens_apparel: true, annual_revenue: 1260000)
+store3 = Store.create(name: "Gastown", mens_apparel: true, womens_apparel: false, annual_revenue: 190000)
 
 puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store4 = Store.create(name: "Surrey", mens_apparel: false, womens_apparel: true, annual_revenue: 224000)
+store5 = Store.create(name: "Whistler", mens_apparel: true, womens_apparel: false, annual_revenue: 1900000)
+store6 = Store.create(name: "Yaletown", mens_apparel: true, womens_apparel: true, annual_revenue: 430000)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.map do |store|
+puts store.name
+end
+
+@womens_stores = Store.where("annual_revenue < '1000000'", womens_apparel: true)
+@womens_stores.map do |store|
+puts store.name
+end
